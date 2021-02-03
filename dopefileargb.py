a8f92db376bbac9e4e08461a7bfaec9398a7587e
ryanbekabe <bekabeipa@gmail.com>
2021-02-03 11:26:22 +0700

    Create dopefileargb.py

---
+++ dopefileargb.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+path = "./exe2/"
+arr = os.listdir(path)
+arr.sort()
+i=0
+for filenya in arr:
+        i=i+1
+        SAMPLE_FILE = filenya
+        print("File ke: " + str(i) + " | " + SAMPLE_FILE)
+        if not os.path.isdir(path + SAMPLE_FILE):
+                os.system("python3 pefileargb.py " + path + SAMPLE_FILE);
+# + " > " + SAMPLE_FILE + "_pefiledump");
